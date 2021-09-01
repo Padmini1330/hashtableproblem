6e27d69f5500d8cc1f4ea2b36e858ef1a3c3effa
padmini v <padmini.v@perfios.com>
2021-09-01 16:53:45 +0530

    ADDED: HashMapMain class to get the frequency of words in a sentence

--- src/com/bridgelabz/hashtableproblem/HashMapMain.java
+++ src/com/bridgelabz/hashtableproblem/HashMapMain.java
@@ -1,5 +1,30 @@
 package com.bridgelabz.hashtableproblem;
 
-public class HashMapMain {
+public class HashMapMain 
+{
+
+	public static void main(String[] args) 
+	{
+		String sentence= "To be or not to be";
+		HashMap<String, Integer> myHashMap = new HashMap<String, Integer>();
+		String [] words = sentence.toLowerCase().split(" ");
+		for(String word:words) 
+		{
+			Integer value = myHashMap.get(word);
+			if(value==null) 
+				value=1;
+			else 
+				value=value+1;
+			myHashMap.add(word, value);
+		}
+		System.out.println("The Frequency of Word *to* is "+myHashMap.get("to"));
+		System.out.println("The Frequency of Word *be* is "+myHashMap.get("be"));
+		System.out.println("The Frequency of Word *or* is "+myHashMap.get("or"));
+		System.out.println("The Frequency of Word *not* is "+myHashMap.get("not"));
+		System.out.println(myHashMap);
+
+	}
+
 
 }
+
