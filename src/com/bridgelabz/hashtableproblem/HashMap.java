4390e349f420b66be499f55e87f13bb67eb425b7
padmini v <padmini.v@perfios.com>
2021-09-01 16:50:17 +0530

    ADDED: methods to add (key,value) pair

--- src/com/bridgelabz/hashtableproblem/HashMap.java
+++ src/com/bridgelabz/hashtableproblem/HashMap.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.hashtableproblem;
 
-public class HashMap {
-
+public class HashMap<K,V> 
+{
+	LinkedList<K> myLinkedList;
+	public HashMap() 
+	{
+		this.myLinkedList = new LinkedList<>();
+	}
+	 public V get(K key) 
+	 {
+		 HashMapNode<K,V> node = (HashMapNode<K, V>) this.myLinkedList.find(key);
+		 return (node==null)?null:node.getValue();
+	 }
+	 public void add(K key,V value) 
+	 {
+		 HashMapNode<K, V> node = (HashMapNode<K, V>) this.myLinkedList.find(key);
+		 if(node == null) 
+		 {
+			 node = new HashMapNode<>(key,value);
+			 this.myLinkedList.append(node);
+		 }
+		 else
+			 node.setValue(value);
+	 }
+	 @Override
+	 public String toString() 
+	 {
+		 return "MyHashMapNodes{"+myLinkedList+'}';
+	 }
 }
