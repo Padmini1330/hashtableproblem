d42797a185dee0d78caf73a32ebef596c8e2e164
padmini v <padmini.v@perfios.com>
2021-09-01 16:51:25 +0530

    ADDED: HashMapNode class which implements INode interface

--- src/com/bridgelabz/hashtableproblem/HashMapNode.java
+++ src/com/bridgelabz/hashtableproblem/HashMapNode.java
@@ -1,5 +1,60 @@
 package com.bridgelabz.hashtableproblem;
 
-public class HashMapNode {
+public class HashMapNode<K,V> implements INode<K> 
+{
+	K key;
+	V value;
+	HashMapNode<K,V> next;
+	HashMapNode(K key,V value)
+	{
+		this.key=key;
+		this.value=value;
+		next=null;
+	}
+	
+	@Override
+	public K getKey() 
+	{
+		return key;
+	}
+
+	@Override
+	public void setKey(K key) 
+	{
+		this.key=key;	
+	}
+
+	@Override
+	public INode getNext() 
+	{
+		return next;
+	}
+
+	@Override
+	public void setNext(INode next) 
+	{
+		this.next=(HashMapNode<K, V>) next;
+		
+	}
+	
+	public V getValue() 
+	{
+		return value;
+	}
+	
+	public void setValue(V value) 
+	{
+		this.value=value;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		StringBuilder myNodeString = new StringBuilder();
+		myNodeString.append("myMapNode{" + "K = ").append(key).append(" , V = ").append(value).append("}");
+		if(next!=null) 
+			myNodeString.append("->").append(next);
+		return myNodeString.toString();
+	}
 
 }
