c0a13c6ef3a8119e08ef2539bd3e65064a8c89f9
padmini v <padmini.v@perfios.com>
2021-09-01 16:46:16 +0530

    ADDED: append and search methods in LinkedList

---
+++ src/com/bridgelabz/hashtableproblem/LinkedList.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.hashtableproblem;
+
+public class LinkedList<K>  
+{
+	K key;
+	INode head,tail;
+	public LinkedList() 
+	{
+		this.head=null;
+		this.tail=null;
+	}
+	
+	public INode find(K key) 
+	{
+		int index = 1;
+		INode currentNode = head;
+		while (currentNode != null) 
+		{
+			if (currentNode.getKey().equals(key)) 
+			{
+				return currentNode;
+			}
+			currentNode = currentNode.getNext();
+			index++;
+		}
+		return null;
+	}
+	
+	
+	public void append(INode<K> newNode) 
+	{
+		if (tail == null)
+			this.tail = newNode;
+		if (head == null)
+			this.head = newNode;
+		else
+			tail.setNext(newNode);
+		tail = newNode;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		 return "MyHashMapNodes{"+head+'}';
+	}
+
+
+}
